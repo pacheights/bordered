e1f17531f43cab96ee634fe983e4f8d5be8a4b86
pacheights <pearsonhbuck@gmail.com>
2021-11-07 12:22:43 -0800

    Brand select

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # vignette
+
+## running the app
+
+to run the app in its current state, navigate to the web directory and run the
+command `npm i; npm start`
--- web/src/components/Form.tsx
+++ web/src/components/Form.tsx
@@ -2,12 +2,13 @@ import { ReactElement, useState } from 'react';
 import styled from 'styled-components';
 import { Photo } from '.';
 import { PhotoBrand } from '../helpers/constants';
+import { BrandSelect } from './BrandSelect';
 
 export function Form(): ReactElement {
   const [img, setImg] = useState(null);
-  const [brand] = useState(PhotoBrand.Polaroid);
+  const [brand, setBrand] = useState(PhotoBrand.Polaroid);
 
-  const onInput = (e: any) => {
+  const onPhotoUpload = (e: any) => {
     const reader = new FileReader();
     reader.onload = (e: any) => {
       setImg(e.target.result);
@@ -15,9 +16,16 @@ export function Form(): ReactElement {
     reader.readAsDataURL(e.target.files[0]);
   };
 
+  const onBrandSelect = (e: any) => setBrand(e.target.value as PhotoBrand);
+
   return (
     <form>
-      <Input type='file' accept='image/png, image/jpeg' onInput={onInput} />
+      <Input
+        type='file'
+        accept='image/png, image/jpeg'
+        onInput={onPhotoUpload}
+      />
+      <BrandSelect onBrandSelect={onBrandSelect} />
       <Photo img={img} brand={brand} />
     </form>
   );
