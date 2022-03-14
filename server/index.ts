7fd864770885136c51d5305a1d6876575dac1c88
pacheights <pearsonhbuck@gmail.com>
2022-03-13 20:42:21 -0700

    server init + delete lambda

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../web', 'build')));
+app.use(express.static('public'));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../web', 'build', 'index.html'));
+});
+
+// start express server on port 3000
+app.listen(3000, () => {
+  console.log('server started on port 3000');
+});
